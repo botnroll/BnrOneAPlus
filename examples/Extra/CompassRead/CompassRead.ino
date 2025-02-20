3f17d472c34ff4c2bcf20fca5adc5ecce3aeb0ec
Trifasico <nandinh99@gmail.com>
2025-02-20 15:54:10 +0000

    First Version of BnrCompass

--- examples/Extra/CompassRead/CompassRead.ino
+++ examples/Extra/CompassRead/CompassRead.ino
@@ -17,84 +17,33 @@
 #include <SPI.h>  // SPI communication library required by BnrOneAPlus.cpp
 #include <Wire.h>
 
-BnrOneAPlus one;  // object to control the Bot'n Roll ONE A+
+BnrOneAPlus one;    // declaration of object variable to control the Bot'n Roll ONE A+
+BnrCompass compass; // declaration of object variable to control the CMPS11 compass
 
 // constants definition
 #define SSPIN 2       // Slave Select (SS) pin for SPI communication
 #define ADDRESS 0x60  // CMPS11 I2C address
 
-void setup() {
-  Wire.begin();  // start I2C BUS
-  Serial.begin(115200);
-  one.spiConnect(SSPIN);  // start SPI communication module
-  one.stop();             // stop motors
+void setup()
+{
+  Serial.begin(57600);
+  one.spiConnect(SSPIN);   // start SPI communication module
+  one.stop();              // stop motors
+  compass.i2cConnect(ADDRESS);
 }
 
-float readBearing() {
-  byte high_byte, low_byte;  // high_byte and low_byte store the bearing and
-                             // fine stores decimal place of bearing
+void loop()
+{
+   float bearing;
+   char roll, pitch;
 
-  Wire.beginTransmission(ADDRESS);  // start communication with CMPS11
-  Wire.write(2);  // Send the register we wish to start reading from
-  Wire.endTransmission();
+   bearing = compass.read_bearing();
+   roll = compass.read_roll();
+   pitch = compass.read_pitch();
 
-  Wire.requestFrom(ADDRESS, 2);  // Request 4 bytes from CMPS11
-  while (Wire.available() < 2);  // Wait for bytes to become available
-  high_byte = Wire.read();
-  low_byte = Wire.read();
-
-  return (float)((high_byte << 8) + low_byte) / 10;
-}
-
-char readRoll() {
-  char roll;  // Store  roll values of CMPS11, chars are used because they
-              // support signed value
-
-  Wire.beginTransmission(ADDRESS);  // start communication with CMPS11
-  Wire.write(5);  // Send the register we wish to start reading from
-  Wire.endTransmission();
-
-  Wire.requestFrom(ADDRESS, 1);  // Request 4 bytes from CMPS11
-  while (Wire.available() < 1);  // Wait for bytes to become available
-  roll = Wire.read();
-
-  return roll;
-}
-
-char readPitch() {
-  char pitch;  // Store pitch values of CMPS11, chars are used because they
-               // support signed value
-
-  Wire.beginTransmission(ADDRESS);  // start communication with CMPS11
-  Wire.write(4);  // Send the register we wish to start reading from
-  Wire.endTransmission();
-
-  Wire.requestFrom(ADDRESS, 1);  // Request 4 bytes from CMPS11
-  while (Wire.available() < 1);  // Wait for bytes to become available
-  pitch = Wire.read();
-
-  return pitch;
-}
-
-void loop() {
-  float bearing;
-  char roll, pitch;
-  char temp[20];
-
-  bearing = readBearing();
-  roll = readRoll();
-  pitch = readPitch();
-
-  Serial.print("Bearing:");
-  Serial.print(bearing);
-  Serial.print("   roll:");
-  Serial.print((int)roll);
-  Serial.print("   pitch:");
-  Serial.println((int)pitch);
-
-  one.lcd1("Bearing: ", bearing);
-  sprintf(temp, "Rol:%d Pit:%d      ", (int)roll, (int)pitch);
-  one.lcd2(temp);
-
-  delay(50);
+   Serial.print("Bearing:"); Serial.print(bearing);
+   Serial.print("   roll:"); Serial.print((int)roll);
+   Serial.print("   pitch:"); Serial.println((int)pitch);
+    
+   delay(50);
 }
