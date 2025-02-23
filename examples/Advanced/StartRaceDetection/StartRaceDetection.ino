07b86657b1638a6831ad5fe59784106436539a02
Nino Pereira <ninopereira.pt@gmail.com>
2025-02-23 12:40:23 +0000

    refactor: minor code refactoring for better readability in start race detection

--- examples/Advanced/StartRaceDetection/StartRaceDetection.ino
+++ examples/Advanced/StartRaceDetection/StartRaceDetection.ino
@@ -1,22 +1,22 @@
 /*
- This example was created by José Cruz (www.botnroll.com)
- on 18 December 2024
-
- This code example is in the public domain.
- http://www.botnroll.com
-
-Description:
-This program detects automatic start on the race challenge.
+ * This example was created by José Cruz (www.botnroll.com)
+ * on 18 December 2024
+ *
+ * This code example is in the public domain.
+ * http://www.botnroll.com
+ *
+ * Description:
+ * This program detects automatic start on the race challenge.
 */
 
 #include <BnrOneAPlus.h>  // Bot'n Roll ONE A+ library
 #include <EEPROM.h>       // EEPROM reading and writing
 #include <SPI.h>  // SPI communication library required by BnrOneAPlus.cpp
-BnrOneAPlus
-    one;  // declaration of object variable to control the Bot'n Roll ONE A+
+BnrOneAPlus one;  // object to control the Bot'n Roll ONE A+
 
 // constants definition
 #define SSPIN 2  // Slave Select (SS) pin for SPI communication
+#define OFF 0
 
 void setup() {
   one.spiConnect(SSPIN);             // start SPI communication module
@@ -34,15 +34,16 @@ void setup() {
 }
 
 bool automaticStart() {
-  bool current_state = one.readIRSensors();  // read actual IR sensors state
+  bool green_light = one.readObstacleSensors();  // read actual IR sensors state
 
-  if (!current_state)  // If state is LOW
+  if (!green_light)
   {
     unsigned long int tempo_A = millis();  // read time
-    while (!current_state)                 // while state is LOW
+    while (!green_light)
     {
-      current_state = one.readIRSensors();  // read actual IR sensors state
-      if ((millis() - tempo_A) > 50)        // if time is low for more than 50ms
+      green_light = one.readObstacleSensors();  // read actual IR sensors state
+      // if 'green light' detected for more than 50ms
+      if ((millis() - tempo_A) > 50)
       {
         return true;  // start Race
       }
